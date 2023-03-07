e301710aa534db0e7ebe31f424d6753b753fea92
Iker Valcarcel <ikervalcarcel@gmail.com>
2023-03-07 15:07:48 +0100

    Primer commit

---
+++ app/src/main/java/com/example/savvysaver/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.savvysaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
